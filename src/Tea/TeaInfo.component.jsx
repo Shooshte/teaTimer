e2c2a44ebabf7bc22883863acf1b62b59fbd73e1
Miha Šušteršič <miha.sustersic.work@gmail.com>
2017-03-23 15:06:28 +0100

    added passing seconds to timer when clicking brew time buttons

--- src/Tea/TeaInfo.component.jsx
+++ src/Tea/TeaInfo.component.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 import './TeaInfo.css';
 
 class TeaInfo extends Component {
+
+  redirectToTimer = (seconds) => {
+    let encodedSeconds = encodeURIComponent(seconds);
+    window.location.hash = '#/timer?seconds' + encodedSeconds;
+  }
+
   renderTeaInfo = (teaData) => {
     return teaData.map((teaType) => {
       return (
@@ -14,8 +20,18 @@ class TeaInfo extends Component {
             <h3 className="title brew-time-title">Recommended brew time:</h3>
           <div className="brew-time-text-container">
             {/*TODO link to timer route passing the time as a parameter*/}
-            <button className="button brew-time-text">Mild: {teaType.brewTime.mild}&nbsp;min.</button>
-            <button className="button brew-time-text">Strong: {teaType.brewTime.strong}&nbsp;min.</button>
+            <button
+              className="button brew-time-text"
+              onClick={() => this.redirectToTimer(teaType.brewTime.mild * 60)}
+            >
+              Mild: {teaType.brewTime.mild}&nbsp;min.
+            </button>
+            <button
+              className="button brew-time-text"
+              onClick={() => this.redirectToTimer(teaType.brewTime.strong * 60)}
+            >
+              Strong: {teaType.brewTime.strong}&nbsp;min.
+            </button>
           </div>
             <div className="tea-text-container">
               {this.renderTeaText(teaType.text)}
