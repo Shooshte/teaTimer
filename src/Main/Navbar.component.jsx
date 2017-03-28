e3c0b0bfa8dfab73db7e0e0ca1ee43f645f29cf0
shooshte <miha.sustersic.work@gmail.com>
2017-03-28 17:52:10 +0200

    removed unnecessary Component, updated the onlyActiveOnIndex prop code

--- src/Main/Navbar.component.jsx
+++ src/Main/Navbar.component.jsx
@@ -11,7 +11,7 @@ const Navbar = ({linksData}) => {
           <div className="navbar-link-container" key={linkData.to}>
             <Link
               activeClassName="navbar-active-link"
-              onlyActiveOnIndex={linkData.to === '/' ? true : false}
+              onlyActiveOnIndex={linkData.to === '/'}
               to={linkData.to}>
               <i className="navbar-icon material-icons">{linkData.icon}</i>
               <span className="navbar-link-text">{linkData.text}</span>
